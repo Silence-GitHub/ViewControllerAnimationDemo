7a0af0dafbcb6dc1fb3a48f26a3b9d2d1e671fce
Kaibo Lu <lukaibolkb@gmail.com>
2017-08-21 20:18:16 +0800

    Add present interactive transition

--- ViewControllerAnimationDemo/TableViewController.swift
+++ ViewControllerAnimationDemo/TableViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class TableViewController: UITableViewController, UIViewControllerTransitioningDelegate {
 
+    private var interactionController: UIPercentDrivenInteractiveTransition?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 100, right: 0)
+        
+        let interactView = UIView(frame: CGRect(x: 0, y: view.bounds.height - 100, width: view.bounds.width, height: 100))
+        interactView.backgroundColor = UIColor.green.withAlphaComponent(0.5)
+        view.addSubview(interactView)
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(interactViewPanned(_:)))
+        interactView.addGestureRecognizer(pan)
+    }
+    
+    @objc private func interactViewPanned(_ pan: UIPanGestureRecognizer) {
+        switch pan.state {
+        case .began:
+            interactionController = UIPercentDrivenInteractiveTransition()
+            
+            let vc = ViewController()
+            vc.transitioningDelegate = self;
+            present(vc, animated: true, completion: nil)
+            
+        case .changed:
+            guard let controller = interactionController else { return }
+            let percent = max(pan.translation(in: pan.view).x, 0) / view.bounds.width
+            controller.update(percent)
+            
+        case .cancelled, .ended:
+            guard let controller = interactionController else { return }
+            // Slow down animation before finishing or canceling
+            // There will be UI bug without slowing down
+            controller.completionSpeed = 0.9
+            if max(pan.translation(in: pan.view).x, 0) / view.bounds.width > 0.5 {
+                // Finish
+                controller.finish()
+            } else {
+                // Cancel
+                controller.cancel()
+            }
+            interactionController = nil
+        default:
+            break
+        }
     }
     
     // MARK: - Table view data source
@@ -78,6 +119,10 @@ class TableViewController: UITableViewController, UIViewControllerTransitioningD
         let animator = VCTransitionAnimator()
         animator.presenting = false
         return animator
+    }
+    
+    func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactionController
     }
 
 }
